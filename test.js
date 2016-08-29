89d7a0227a4140f9bced51582151ef9c978a2d48
Andrew Szucs <andrew0szucs@gmail.com>
2016-08-30 01:23:49 +0200

    Fixed Bootbot repo commit and deleted test.js.

--- test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-const moment = require('moment');
-
-console.log(moment(1473188400000).format('YYYYMMDD[T]HHmmss[Z]').toString());
-console.log(new Date(1473188400000).toString());
